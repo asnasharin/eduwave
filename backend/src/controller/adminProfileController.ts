549dd2be6b3b84dcca4f8d8d23adae6cd84843be
asna sharin <asnasherin1233@gmail.com>
2024-10-22 22:07:05 +0530

    created admin profile controller

---
+++ backend/src/controller/adminProfileController.ts
@@ -0,0 +1,24 @@
+import asynchandler from "express-async-handler";
+import { Request, Response, NextFunction } from "express";
+import Admin from "../model/adminProfileModel";
+import mongoose from "mongoose";
+
+/**
+ * @desc    Get Admin Profile
+ * @route   GET api/admin
+ * @access  private
+ */
+export const getAdminProfile = asynchandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?._id;
+    const userProfile = await Admin.findOne({ userID: userId });
+    if (userProfile) {
+      res.status(200).json({
+        success: true,
+        userProfile: userProfile,
+      });
+    } else {
+      return next(Error("Somthing went wrong"));
+    }
+  }
+);
